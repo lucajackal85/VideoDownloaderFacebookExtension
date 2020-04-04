7596c87670945ffbb55fc8525ec5cd8c976ed72f
Luca <luca.giacalone@edimotive.com>
2020-04-04 14:02:49 +0200

    fix composer requirement

--- src/Exception/FacebookDownloadException.php
+++ src/Exception/FacebookDownloadException.php
@@ -11,7 +11,7 @@ class FacebookDownloadException extends DownloadException
         return new FacebookDownloadException('No video URLs found');
     }
 
-    public static function formatNotFound(array $selectedFormats, array $available)
+    public static function formatNotFound(array $selectedFormats, array $available) : FacebookDownloadException
     {
         return new FacebookDownloadException(sprintf(
             'Format%s %s is not available. [Available formats are: %s]',
